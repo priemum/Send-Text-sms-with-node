fdaa7e82279297a23c15f66ef0bed2a906f19542
priemum <50713962+priemum@users.noreply.github.com>
2021-08-28 21:25:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const bodyParser=require('body-parser')
 const ejs=require('ejs')
-const Nexmo=require('nexmo')
+const Nexmo=require('nexmo') 
 const socketio=require('socket.io')
 
 
